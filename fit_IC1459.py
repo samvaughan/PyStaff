6901aef029c2c9bbeb37185cc5190f23de094db9
Sam Vaughan <sam.vaughan@physics.ox.ac.uk>
2018-05-22 12:00:11 +0100

    Bug in MPI pool lines

--- fit_IC1459.py
+++ fit_IC1459.py
@@ -154,126 +154,129 @@ print 'Done all the preliminaries'
 
 # #Set up the parameters
 with MPIPool() as pool:
-    if not pool.is_master():
-
-
-        print 'Setting up the fit'
-        fit=SpectralFitting.SpectralFit(lamdas, flux, errors, pixel_weights, fit_wavelengths, FWHM_gal, instrumental_resolution=instrumental_resolution, skyspecs=None, element_imf=element_imf)
-        fit.set_up_fit()
-        parameters=fit.fit_settings
-
-        theta=LMSPV.Parameters()
-        theta.add('Vel', value=1800.91, min=-1000.0, max=10000.0)
-        theta.add('sigma', value=330.0, min=10.0, max=500.0)
-
-        theta.add('Na', value=0.0, min=-0.45, max=1.0, vary=True)
-
-        theta.add('Ca', value=0.0,  min=-0.45, max=0.45, vary=True)
-        theta.add('Fe', value=0.0, min=-0.45, max=0.45, vary=True)
-        theta.add('C', value=0.0, min=-0.45, max=0.45, vary=True)
-        theta.add('N', value=0.0, min=-0.45, max=0.45, vary=True)
-        theta.add('Ti', value=0.0, min=-0.45, max=0.45, vary=True)
-        theta.add('Mg', value=0.0, min=-0.45, max=0.45, vary=True)
-        theta.add('Si', value=0.0, min=-0.45, max=0.45, vary=True)
-        theta.add('Ba', value=0.0, min=-0.45, max=0.45, vary=True)
-
-
-        theta.add('as_Fe', value=0.0, min=0.0, max=0.45, vary=True)
-        theta.add('Cr', value=0.0, min=0.0, max=0.45, vary=False)
-        theta.add('Mn', value=0.0, min=0.0, max=0.45, vary=False)
-        theta.add('Ni', value=0.0, min=0.0, max=0.45, vary=False)
-        theta.add('Co', value=0.0, min=0.0, max=0.45, vary=False)
-        theta.add('Eu', value=0.0, min=0.0, max=0.45, vary=False)
-        theta.add('Sr', value=0.0, min=0.0, max=0.45, vary=False)
-        theta.add('K', value=0.0, min=0.0, max=0.45, vary=False)
-        theta.add('V', value=0.0, min=0.0, max=0.45, vary=False)
-        theta.add('Cu', value=0.0, min=0.0, max=0.45, vary=False)
-
-
-        theta.add('Vel_em', value=1800.91, min=0.0, max=10000)
-        theta.add('sig_em', value=200.0, min=10.0, max=500.0)
-
-        #These are log flux- they get exponentiated in the likelihood function
-        theta.add('Ha', value=-1.5, min=-10.0, max=10.0)
-        theta.add('Hb', value=-2.0, min=-10.0, max=10.0)
-        theta.add('NII', value=-1.0, min=-10.0, max=10.0)
-        theta.add('SII_6716', value=-2.0, min=-10.0, max=10.0)
-        theta.add('SII_6731', value=-2.0, min=-10.0, max=10.0)
-        theta.add('OIII', value=-2.0, min=-10.0, max=10.0)
-        theta.add('OI', value=-2.0, min=-10.0, max=10.0)
-
-        theta.add('age', value=10.0, min=1.0, max=14.0)
-        theta.add('Z', value=0.0, min=-1.0, max=0.2)
-        theta.add('imf_x1', value=2.35, min=0.5, max=3.5)
-        theta.add('imf_x2', value=2.35, min=0.5, max=3.5) 
-
-        theta.add('O2_Scale', value=-335.70307655300002, min=-100000000, max=100000000, vary=False) 
-        theta.add('sky_Scale', value=988.58729658499999, min=-100000000, max=100000000, vary=False) 
-        theta.add('OH_Scale', value=-132.30995387499999, min=-100000000, max=100000000, vary=False) 
-        theta.add('NaD_sky_scale', value=-335.70307655300002, min=-100000000, max=100000000, vary=False)
-
-        theta.add('ln_f', value=0.0, min=-5.0, max=5.0, vary=True)
-
-        #Select the parameters we're varying, ignore the fixed ones
-        variables=[thing for thing in theta if theta[thing].vary]
-        ndim=len(variables)
-        #Vice versa, plus add in the fixed value
-        fixed=[ "{}={},".format(thing, theta[thing].value) for thing in theta if not theta[thing].vary]
-        nwalkers=200
-        nsteps=30000
+        if not pool.is_master():
+        pool.wait()
+        sys.exit(0)
 
 
 
-        #Get the spread of the starting positions
-        stds=[]
-        n_general=9
-        n_positive=1
-        n_emission_lines=7
+    print 'Setting up the fit'
+    fit=SpectralFitting.SpectralFit(lamdas, flux, errors, pixel_weights, fit_wavelengths, FWHM_gal, instrumental_resolution=instrumental_resolution, skyspecs=None, element_imf=element_imf)
+    fit.set_up_fit()
+    parameters=fit.fit_settings
 
-        #Kinematic parameters
-        stds.extend([100.0, 50.0])
-        #General parameters
-        stds.extend([0.1]*n_general)
-        #Positive parameters
-        stds.extend([0.1]*n_positive)
+    theta=LMSPV.Parameters()
+    theta.add('Vel', value=1800.91, min=-1000.0, max=10000.0)
+    theta.add('sigma', value=330.0, min=10.0, max=500.0)
 
-        #Emission lines
-        stds.extend([100.0, 50.0])
-        stds.extend([1.0]*n_emission_lines)
+    theta.add('Na', value=0.0, min=-0.45, max=1.0, vary=True)
 
-
-        #Age
-        stds.extend([1.0])
-        #Z, imf1, imf2
-        stds.extend([0.1, 0.1, 0.1])
-        #Sky
-        #stds.extend([100.0,  100.0,  100.0, 100.0])
-        #ln_f
-        stds.extend([0.5])
-
-        stds=np.array(stds)
+    theta.add('Ca', value=0.0,  min=-0.45, max=0.45, vary=True)
+    theta.add('Fe', value=0.0, min=-0.45, max=0.45, vary=True)
+    theta.add('C', value=0.0, min=-0.45, max=0.45, vary=True)
+    theta.add('N', value=0.0, min=-0.45, max=0.45, vary=True)
+    theta.add('Ti', value=0.0, min=-0.45, max=0.45, vary=True)
+    theta.add('Mg', value=0.0, min=-0.45, max=0.45, vary=True)
+    theta.add('Si', value=0.0, min=-0.45, max=0.45, vary=True)
+    theta.add('Ba', value=0.0, min=-0.45, max=0.45, vary=True)
 
 
+    theta.add('as_Fe', value=0.0, min=0.0, max=0.45, vary=True)
+    theta.add('Cr', value=0.0, min=0.0, max=0.45, vary=False)
+    theta.add('Mn', value=0.0, min=0.0, max=0.45, vary=False)
+    theta.add('Ni', value=0.0, min=0.0, max=0.45, vary=False)
+    theta.add('Co', value=0.0, min=0.0, max=0.45, vary=False)
+    theta.add('Eu', value=0.0, min=0.0, max=0.45, vary=False)
+    theta.add('Sr', value=0.0, min=0.0, max=0.45, vary=False)
+    theta.add('K', value=0.0, min=0.0, max=0.45, vary=False)
+    theta.add('V', value=0.0, min=0.0, max=0.45, vary=False)
+    theta.add('Cu', value=0.0, min=0.0, max=0.45, vary=False)
 
 
-        start_values, bounds=SF.get_start_vals_and_bounds(theta)
-        p0=SF.get_starting_poitions_for_walkers(start_values, stds, nwalkers)
-        #CHeck everything is within the bounds
-        #Make sure the positive parameters stay positive
-        p0[2+n_general:2+n_general+n_positive, :]=np.abs(p0[2+n_general:2+n_general+n_positive, :])
-        #This checks to see if any rows of the array have values which are too high, and replaces them with the upper bound value
-        #Add the machine epsilon to deal with cases where we end up with, for example, one walker set to be -0.20000000000000001 instead of -0.2
-        p0[p0<bounds[:, 0, None]]=bounds[np.any(p0<bounds[:, 0, None], axis=1), 0]+10*np.finfo(np.float64).eps
-        #And the same for any values which are too low
-        p0[p0>bounds[:, 1, None]]=bounds[np.any(p0>bounds[:, 1, None], axis=1), 1]-10*np.finfo(np.float64).eps
+    theta.add('Vel_em', value=1800.91, min=0.0, max=10000)
+    theta.add('sig_em', value=200.0, min=10.0, max=500.0)
 
-        assert np.all((p0>bounds[:, 0, None])&(p0<bounds[:, 1, None])), 'Some walkers are starting in bad places of parameter space!'
+    #These are log flux- they get exponentiated in the likelihood function
+    theta.add('Ha', value=-1.5, min=-10.0, max=10.0)
+    theta.add('Hb', value=-2.0, min=-10.0, max=10.0)
+    theta.add('NII', value=-1.0, min=-10.0, max=10.0)
+    theta.add('SII_6716', value=-2.0, min=-10.0, max=10.0)
+    theta.add('SII_6731', value=-2.0, min=-10.0, max=10.0)
+    theta.add('OIII', value=-2.0, min=-10.0, max=10.0)
+    theta.add('OI', value=-2.0, min=-10.0, max=10.0)
+
+    theta.add('age', value=10.0, min=1.0, max=14.0)
+    theta.add('Z', value=0.0, min=-1.0, max=0.2)
+    theta.add('imf_x1', value=2.35, min=0.5, max=3.5)
+    theta.add('imf_x2', value=2.35, min=0.5, max=3.5) 
+
+    theta.add('O2_Scale', value=-335.70307655300002, min=-100000000, max=100000000, vary=False) 
+    theta.add('sky_Scale', value=988.58729658499999, min=-100000000, max=100000000, vary=False) 
+    theta.add('OH_Scale', value=-132.30995387499999, min=-100000000, max=100000000, vary=False) 
+    theta.add('NaD_sky_scale', value=-335.70307655300002, min=-100000000, max=100000000, vary=False)
+
+    theta.add('ln_f', value=0.0, min=-5.0, max=5.0, vary=True)
+
+    #Select the parameters we're varying, ignore the fixed ones
+    variables=[thing for thing in theta if theta[thing].vary]
+    ndim=len(variables)
+    #Vice versa, plus add in the fixed value
+    fixed=[ "{}={},".format(thing, theta[thing].value) for thing in theta if not theta[thing].vary]
+    nwalkers=200
+    nsteps=30000
 
 
-        fname='attempt1_centre.h5'
-        backend=emcee3.backends.HDFBackend(fname)
 
-        sampler = emcee3.EnsembleSampler(nwalkers, ndim, lnprob, args=[theta, variables, bounds], backend=backend, pool=None)
-        result=sampler.run_mcmc(p0.T, nsteps, progress=True)
+    #Get the spread of the starting positions
+    stds=[]
+    n_general=9
+    n_positive=1
+    n_emission_lines=7
 
-        #SF.lnlike(theta, fit.fit_settings)
+    #Kinematic parameters
+    stds.extend([100.0, 50.0])
+    #General parameters
+    stds.extend([0.1]*n_general)
+    #Positive parameters
+    stds.extend([0.1]*n_positive)
+
+    #Emission lines
+    stds.extend([100.0, 50.0])
+    stds.extend([1.0]*n_emission_lines)
+
+
+    #Age
+    stds.extend([1.0])
+    #Z, imf1, imf2
+    stds.extend([0.1, 0.1, 0.1])
+    #Sky
+    #stds.extend([100.0,  100.0,  100.0, 100.0])
+    #ln_f
+    stds.extend([0.5])
+
+    stds=np.array(stds)
+
+
+
+
+    start_values, bounds=SF.get_start_vals_and_bounds(theta)
+    p0=SF.get_starting_poitions_for_walkers(start_values, stds, nwalkers)
+    #CHeck everything is within the bounds
+    #Make sure the positive parameters stay positive
+    p0[2+n_general:2+n_general+n_positive, :]=np.abs(p0[2+n_general:2+n_general+n_positive, :])
+    #This checks to see if any rows of the array have values which are too high, and replaces them with the upper bound value
+    #Add the machine epsilon to deal with cases where we end up with, for example, one walker set to be -0.20000000000000001 instead of -0.2
+    p0[p0<bounds[:, 0, None]]=bounds[np.any(p0<bounds[:, 0, None], axis=1), 0]+10*np.finfo(np.float64).eps
+    #And the same for any values which are too low
+    p0[p0>bounds[:, 1, None]]=bounds[np.any(p0>bounds[:, 1, None], axis=1), 1]-10*np.finfo(np.float64).eps
+
+    assert np.all((p0>bounds[:, 0, None])&(p0<bounds[:, 1, None])), 'Some walkers are starting in bad places of parameter space!'
+
+
+    fname='attempt1_centre.h5'
+    backend=emcee3.backends.HDFBackend(fname)
+
+    sampler = emcee3.EnsembleSampler(nwalkers, ndim, lnprob, args=[theta, variables, bounds], backend=backend, pool=None)
+    result=sampler.run_mcmc(p0.T, nsteps, progress=True)
+
+    #SF.lnlike(theta, fit.fit_settings)
